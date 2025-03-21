19de5535a88807f2cacd65133bd79602a607bdeb
densenden <git@deniskreuzer.dk>
2025-03-21 11:06:13 +0100

    not found info rendered to template

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -26,30 +26,34 @@ def string_creator(animal_data):
         """
     return animals_string
 
-def html_replacer(animals_data):
-    """
-    Replace the placeholder in the HTML template with the animal data.
 
-    Args:
-        animals_data (list): A list of dictionaries containing animal information.
+def html_replacer(animals_data=None, error_message=None):
+    """
+    Replace the placeholder in the HTML template with the animal data or an error message.
     """
     with open("animals_templates.html", "r") as html_object:
         placeholder = html_object.read()
 
-    new_content = placeholder.replace("__REPLACE_ANIMALS_INFO__", string_creator(animals_data))
+    if error_message:
+        new_content = placeholder.replace("__REPLACE_ANIMALS_INFO__", f"<h2>{error_message}</h2>")
+    else:
+        new_content = placeholder.replace("__REPLACE_ANIMALS_INFO__", string_creator(animals_data))
 
     with open("animals.html", "w") as new_file:
         new_file.write(new_content)
+
 
 def main():
     while True:
         animal_name = input("Enter a name of an animal: ")
         animal_data = fetch_data(animal_name)
         if animal_data:
-            html_replacer(animal_data)
+            html_replacer(animals_data=animal_data)
             print(f"Website was successfully generated to the file animals.html for {animal_name}.")
             break
         else:
+            error_message = f'The animal "{animal_name}" doesn\'t exist.'
+            html_replacer(error_message=error_message)
             print(f"No data found for the given animal name {animal_name}. Please try again.")
 
 
